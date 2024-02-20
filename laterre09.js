9787bf5c9dbd596a6237f2ff7adac8ed3ff0574a
moussa sacko <moussasacko418@gmail.com>
2024-02-20 22:47:52 +0100

    gestion d'erreur

--- laterre09.js
+++ laterre09.js
@@ -3,7 +3,12 @@ var arguments = process.argv[2];
 var racine = arguments
 var precision = 0.00001;
 
-while (Math.abs(arguments - racine*racine) > precision) {
+
+if (isNaN(arguments) || process.argv[3] != undefined) {
+    console.log("Erreur")
+} else {
+    while (Math.abs(arguments - racine*racine) > precision) {
     racine = (racine + arguments / racine) / 2
+    }
+    console.log(Math.floor(racine))
 }
-console.log(Math.floor(racine))
